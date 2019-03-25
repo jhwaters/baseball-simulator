42b1d8d4983a90129eb4140a488850c8034a73ea
JW <>
2019-03-25 11:22:08 -0400

    Updates

---
+++ precache-manifest.f9ce27c07d28861f8b4756df855c44cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c795fcf3c969dbfbe2df",
+    "url": "/baseball-simulator/static/css/main.928f26f0.chunk.css"
+  },
+  {
+    "revision": "c795fcf3c969dbfbe2df",
+    "url": "/baseball-simulator/static/js/main.cbfce3b9.chunk.js"
+  },
+  {
+    "revision": "e9060d21ca071fe8c58a",
+    "url": "/baseball-simulator/static/js/runtime~main.07d0cb16.js"
+  },
+  {
+    "revision": "3c82eb2a47830b799ee4",
+    "url": "/baseball-simulator/static/js/2.4df5e306.chunk.js"
+  },
+  {
+    "revision": "103b43f65c5611b6bc5942364f950e16",
+    "url": "/baseball-simulator/index.html"
+  }
+];
